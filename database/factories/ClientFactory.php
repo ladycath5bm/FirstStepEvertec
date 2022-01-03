82d8f72d8dd0e65cf48133c24e57cc88bf91c3e4
ladycath5bm <leidycath.bm@gmail.com>
2022-01-02 21:33:13 -0500

    feat:factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use phpDocumentor\Reflection\Types\This;
+use Illuminate\Support\Str;
 
 class ClientFactory extends Factory
 {
@@ -14,7 +16,29 @@ class ClientFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'country' => $this->faker->country(),
+            'city' => $this->faker->city(),
+            'adress' => $this->faker->adress(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
     }
 }
